cdeb7658a6373f7d85655e2efc7718ad6cf2d7a5
Jake Smiley <jake.alan.smiley@gmail.com>
2020-10-10 21:02:50 -0600

    intern tests

---
+++ __tests__/Intern.test.js
@@ -0,0 +1,16 @@
+const { test, expect } = require('@jest/globals');
+const Intern = require('../lib/Intern');
+
+//confirm school is set
+test("Set school for Intern", () => {
+    const employee = new Intern('Joshua', '21', 'test@testing.com', 'UofU');
+
+    expect(employee.getSchool()).toBe("UofU");
+});
+
+//confirm Intern role
+test("confirm that getRole() returns Intern", () => {
+    const employee = new Intern('Joshua', '21', 'test@testing.com', 'UofU');
+
+    expect(employee.getRole()).toBe("UofU");
+});
