1a053f4a9c4bd455b29d04a02d2ab5378fd544e1
Jake Smiley <jake.alan.smiley@gmail.com>
2020-10-12 17:56:19 -0600

    making sure github is up to date

--- Index.js
+++ Index.js
@@ -43,6 +43,8 @@ const teamBuilder = () => {
             console.log(managerInstance);
 
         });
+
+        userChoice();
     };
 
     // 1.5 Controller's choice
@@ -69,18 +71,78 @@ const teamBuilder = () => {
                 };
                 if (selectedChoice === 'Done adding team members') {
                     genHtml();
-                }
+                };
             });
     };
 
     // 2. Add Intern
-    const addIntern = () => { };
+    const addIntern = () => {
+        prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is the name of the intern?'
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'What is the Employee ID number of the intern?'
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'What is the email address of the intern?'
+            },
+            {
+                type: 'input',
+                name: 'school',
+                message: 'What school does the intern go to?'
+            }
+            
+        ]).then((internInfo) => {
+            const internInstance = new Intern(internInfo.name, internInfo.id, internInfo.email, internInfo.school);
+            console.log(internInstance);
+        });
+
+        userChoice();
+    };
 
     // 3. Add Engineer
-    const addEngineer = () => { };
+    const addEngineer = () => {
+        prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is the name of the engineer?'
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'What is the Employee ID number of the engineer?'
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'What is the email address of the engineer?'
+            },
+            {
+                type: 'input',
+                name: 'github',
+                message: 'What is the engineer\'s GitHub?'
+            }
+            
+        ]).then((engineerInfo) => {
+            const engineerInstance = new Engineer(engineerInfo.name, engineerInfo.id, engineerInfo.email, engineerInfo.school);
+            console.log(engineerInstance);
+        });
+
+        userChoice();
+    };
 
     // 4. Gen HTML
-    const genHtml = () => { };
+    const genHtml = () => {
+        
+    };
 
     addManager();
 
